494a012ea2c9a6a4a4ca0c720f0d413ba1c57fc6
ami620 <rrtd_620@outlook.jp>
2024-04-16 16:31:06 +0900

    third commit

--- spring_boot/samuraitravel/src/main/java/com/example/samuraitravel/service/StripeService.java
+++ spring_boot/samuraitravel/src/main/java/com/example/samuraitravel/service/StripeService.java
@@ -1,13 +1,19 @@
 package com.example.samuraitravel.service;
 
+import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.example.samuraitravel.form.ReservationRegisterForm;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
+import com.stripe.model.Event;
+import com.stripe.model.StripeObject;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
+import com.stripe.param.checkout.SessionRetrieveParams;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -15,6 +21,12 @@ import jakarta.servlet.http.HttpServletRequest;
 public class StripeService {
 	@Value("${stripe.api-key}")
     private String stripeApiKey;
+	
+	  private final ReservationService reservationService;
+	     
+	     public StripeService(ReservationService reservationService) {
+	         this.reservationService = reservationService;
+	     }    
 	
 	// セッションを作成し、Stripeに必要な情報を返す
     public String createStripeSession(String houseName, ReservationRegisterForm reservationRegisterForm, HttpServletRequest httpServletRequest) {
@@ -58,4 +70,21 @@ public class StripeService {
         }
     } 
 
+    
+ // セッションから予約情報を取得し、ReservationServiceクラスを介してデータベースに登録する  
+    public void processSessionCompleted(Event event) {
+        Optional<StripeObject> optionalStripeObject = event.getDataObjectDeserializer().getObject();
+        optionalStripeObject.ifPresent(stripeObject -> {
+            Session session = (Session)stripeObject;
+            SessionRetrieveParams params = SessionRetrieveParams.builder().addExpand("payment_intent").build();
+
+            try {
+                session = Session.retrieve(session.getId(), params, null);
+                Map<String, String> paymentIntentObject = session.getPaymentIntentObject().getMetadata();
+                reservationService.create(paymentIntentObject);
+            } catch (StripeException e) {
+                e.printStackTrace();
+            }
+        });
+    }    
 }
