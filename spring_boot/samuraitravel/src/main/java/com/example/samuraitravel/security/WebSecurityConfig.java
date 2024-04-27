b6c59cb724af9befc0b3b41ab48d858296b29fab
ami620 <rrtd_620@outlook.jp>
2024-04-27 14:04:19 +0900

    0427commit

--- spring_boot/samuraitravel/src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
+++ spring_boot/samuraitravel/src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
@@ -8,18 +8,29 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class WebSecurityConfig {
-	
-	@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests((requests) -> requests                
-            	.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**", "/houses", "/houses/{id}", "/stripe/webhook").permitAll()  // すべてのユーザーにアクセスを許可するURL          
-                .requestMatchers("/admin/**").hasRole("ADMIN")  // 管理者にのみアクセスを許可するURL
+            .authorizeHttpRequests((requests) -> requests
+                .requestMatchers(
+                    new AntPathRequestMatcher("/css/**"),
+                    new AntPathRequestMatcher("/images/**"),
+                    new AntPathRequestMatcher("/js/**"),
+                    new AntPathRequestMatcher("/storage/**"),
+                    new AntPathRequestMatcher("/"),
+                    new AntPathRequestMatcher("/signup/**"),
+                    new AntPathRequestMatcher("/houses"),
+                    new AntPathRequestMatcher("/houses/{id}"),
+                    new AntPathRequestMatcher("/stripe/webhook")
+                ).permitAll()  // すべてのユーザーにアクセスを許可するURL           
+                .requestMatchers(new AntPathRequestMatcher("/admin/**")).hasRole("ADMIN")  // 管理者にのみアクセスを許可するURL
                 .anyRequest().authenticated()                   // 上記以外のURLはログインが必要（会員または管理者のどちらでもOK）
             )
             .formLogin((form) -> form
@@ -33,15 +44,16 @@ public class WebSecurityConfig {
                 .logoutSuccessUrl("/?loggedOut")  // ログアウト時のリダイレクト先URL
                 .permitAll()
             )
-            .csrf().ignoringRequestMatchers("/stripe/webhook");
-            
+            .csrf(csrf -> csrf
+                .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/stripe/webhook")) // CSRFチェックを実行するパスを指定
+            );
+
         return http.build();
     }
-    
+
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
-
 }
+
